84f19705751d829329c404a3be33f89bb3bec1c2
Jinseok Jeong <enki74@gmail.com>
2018-01-29 14:59:08 +0900

    283 Who's Pulled the Trigger

--- Source/S05_TestingGrounds/Character/Mannequin.cpp
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -38,6 +38,11 @@ void AMannequin::BeginPlay()
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
+	if (InputComponent != nullptr)
+	{
+		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
+	}
 }
 
 // Called every frame
@@ -54,7 +59,7 @@ void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 
 }
 
-void AMannequin::fire()
+void AMannequin::PullTrigger()
 {
 	Gun->OnFire();
 }
